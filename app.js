38061a4c5b13ac8caf942f6b6b79bcc56ad98c1f
gabriell-ferreira <araujogabriel80@hotmail.com>
2021-06-28 17:10:10 -0400

    Add heal feature and refactor code

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ new Vue({
   el: '#app',
   data: {
     show: true,
-    lifePlayer: 100,
-    lifeMonster: 100,
     history: [],
-    damageMonster: 0,
+    viewMessage: false,
+    winnerMessage: '',
+
+    lifePlayer: 100,
     damagePlayer: 0,
     messagePlayer: '',
-    messageMonster: '',
-    viewMessage: false,
-    winnerMessage: ''
+
+    lifeMonster: 100,
+    damageMonster: 0,
+    messageMonster: ''
   },
   methods: {
     clearHistory(){
@@ -25,31 +27,52 @@ new Vue({
       this.viewMessage = false
       this.show = !this.show
     },
-    quit(){
-      this.clearHistory()
-      this.show = !this.show
-
+    resetLife(){
       this.lifePlayer = 100
       this.lifeMonster = 100
     },
-    attack(bonus){
+    quit(){
+      this.show = !this.show
+      this.resetLife()
+    },
+    attack(){
+      this.playerAttack()
+      this.monsterAttack()
+    },
+    specialAttack(){
+      let bonus = 3
+      this.playerAttack(bonus)
+      this.monsterAttack()
+    },
+    heal(){
+      let lifeHeal = this.getRandomInt(1,12)
+      this.lifePlayer = this.lifePlayer > 0 ? this.lifePlayer += lifeHeal : this.lifePlayer
+
+      let healMessage = 'Player heal with ' + lifeHeal
+
+      this.monsterAttack()
+      this.history.push(healMessage)
+      console.log(this.lifeMonster)
+    },
+    playerAttack(bonus){
       this.damagePlayer = bonus > 1 ? this.getRandomInt(5, 9) * bonus : this.getRandomInt(5, 9)
-      this.damageMonster = this.getRandomInt(8, 12)
-
       this.lifeMonster -= this.damagePlayer
-      this.lifePlayer -= this.damageMonster
-
       this.messagePlayer = `player hit monster whit ${this.damagePlayer}`
-      this.messageMonster = `monster hit player whit ${this.damageMonster}`
 
       this.history.push(this.messagePlayer)
-      this.history.push(this.messageMonster)
-
+      
       if(this.lifeMonster <= 0){
         this.lifeMonster = 0
         this.winner()
         return
       }
+    },
+    monsterAttack(){
+      this.damageMonster = this.getRandomInt(8, 12)
+      this.lifePlayer -= this.damageMonster
+      this.messageMonster = `monster hit player whit ${this.damageMonster}`
+
+      this.history.push(this.messageMonster)
 
       if(this.lifePlayer <= 0){
         this.lifePlayer = 0
@@ -61,16 +84,12 @@ new Vue({
       if(this.lifeMonster == 0){
         this.viewMessage = true
         this.winnerMessage = 'You win!'
-      }
-
-      if(this.lifePlayer == 0){
+        return
+      }else if(this.lifePlayer == 0){
         this.viewMessage = true
         this.winnerMessage = 'You lose!'
+        return
       }
-    },
-    specialAttack(){
-      let bonus = 3
-      this.attack(bonus)
     },
     getRandomInt(min, max){
       return Math.floor(Math.random() * (max - min + 1)) + min
